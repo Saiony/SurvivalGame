b06fbb711e8480b207fc1c389a848e7a51e072d6
Gandhi <ulisses.silva@fanatee.com>
2020-08-02 16:16:31 -0300

    Interactable criado
    também tem uma versão do NPC

---
+++ BrackeysGJ/Assets/Game/Controller/Interactable/Interactable.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+namespace Game.Controller.Interactable
+{
+    [ExecuteInEditMode]
+    public abstract class Interactable : MonoBehaviour
+    {
+        [Range(1, 5f)]
+        public float interactableRange = 1f;
+        private SphereCollider collider;
+        private bool isPlayerInside;
+
+        protected virtual void Start()
+        {
+            if (gameObject.GetComponent<SphereCollider>() == null)
+                gameObject.AddComponent<SphereCollider>();
+            collider = GetComponent<SphereCollider>();
+            collider.isTrigger = true;
+            collider.radius = interactableRange;
+        }
+
+        protected virtual void Update()
+        {
+#if UNITY_EDITOR
+            collider.radius = interactableRange;
+#endif
+            if (isPlayerInside)
+            {
+                if(Input.GetKeyDown(KeyCode.Space))
+                    OnPlayerInteract();
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                isPlayerInside = true;
+                OnPlayerEnter();
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                isPlayerInside = false;
+                OnPlayerExit();
+            }
+        }
+
+        protected abstract void OnPlayerEnter();
+        protected abstract void OnPlayerExit();
+        protected abstract void OnPlayerInteract();
+    }
+}
---
+++ BrackeysGJ/Assets/Game/Controller/NPC/NpcController.cs
@@ -0,0 +1,50 @@
+﻿using TMPro;
+using UnityEngine;
+namespace Game.Controller.NPC
+{
+    public class NpcController : Interactable.Interactable
+    {
+        public TextMeshProUGUI missionStatusMark;
+
+        private bool hasActiveMission;
+        private bool isWaitingPlayerReturnFromQuest;
+
+        void Start()
+        {
+            base.Start();
+            UpdateMissionStatusMark();
+        }
+
+        public void UpdateMissionStatusMark()
+        {
+            if (hasActiveMission)
+            {
+                missionStatusMark.text = "!";
+            }
+        }
+
+        public void LetPlayerStartQuestion()
+        {
+            this.hasActiveMission = true;
+        }
+
+        void Update()
+        {
+            base.Update();
+        }
+
+        protected override void OnPlayerEnter()
+        {
+        }
+
+        protected override void OnPlayerExit()
+        {
+        }
+
+        protected override void OnPlayerInteract()
+        {
+            if (!hasActiveMission)
+                return;
+        }
+    }
+}
