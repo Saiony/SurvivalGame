09b9b63543c211a64fb2ea3fabfe21783327d965
caue-vicente-afterverse <caue.vicente@afterverse.com>
2022-01-27 21:53:50 -0300

    Attemp on camera

--- BrackeysGJ/Assets/Game/Scripts/Controller/CameraController.cs
+++ BrackeysGJ/Assets/Game/Scripts/Controller/CameraController.cs
@@ -2,32 +2,37 @@
 using System.Collections.Generic;
 using Game.Scripts.Controller.Player;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
     [SerializeField]
-    private PlayerController _player = null;
-    private PlayerController Player => _player;
+    private Transform FollowTransform;
 
-    [SerializeField]
-    private float _offsetX = 0;
-    private float OffsetX => _offsetX;
+    private Vector2 Look;
 
-    [SerializeField]
-    private float _offsetY = 0;
-    private float OffsetY => _offsetY;
-
-    [SerializeField]
-    private float _offsetZ = 0;
-    private float OffsetZ => _offsetZ;
-
-    private void FixedUpdate()
+    public void OnLook(InputValue value)
     {
-        transform.position = new Vector3
-        (
-            Player.transform.position.x + OffsetX,
-            Player.transform.position.y + OffsetY,
-            Player.transform.position.z + OffsetZ
-        );
+        _look = value.Get<Vector2>();
+    }
+
+    private void Update()
+    {
+        //Rotate the Follow Target transform based on the input
+        FollowTransform.transform.rotation *= Quaternion.AngleAxis(_look.x * rotationPower, Vector3.up);
+        FollowTransform.transform.rotation *= Quaternion.AngleAxis(_look.y * rotationPower, -Vector3.right);
+
+        var angles = FollowTransform.transform.localEulerAngles;
+        angles.z = 0;
+
+        var angle = FollowTransform.transform.localEulerAngles.x;
+
+        //Clamp rotation
+        if (angle > 180 && angle < 340)
+            angles.x = 340;
+        else if(angle < 180 && angle > 40)
+            angles.x = 40;
+
+        FollowTransform.transform.localEulerAngles = angles;
     }
 }
