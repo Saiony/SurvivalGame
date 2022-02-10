a0aa5c11f3db464cbb3b101672771e29a7acee08
caue-vicente-afterverse <caue.vicente@afterverse.com>
2022-02-09 21:45:15 -0300

    Implement 3rd person camera

--- BrackeysGJ/Assets/Game/Scripts/Controller/CameraController.cs
+++ BrackeysGJ/Assets/Game/Scripts/Controller/CameraController.cs
@@ -2,37 +2,11 @@
 using System.Collections.Generic;
 using Game.Scripts.Controller.Player;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField]
-    private Transform FollowTransform;
-
-    private Vector2 Look;
-
-    public void OnLook(InputValue value)
-    {
-        _look = value.Get<Vector2>();
-    }
 
     private void Update()
     {
-        //Rotate the Follow Target transform based on the input
-        FollowTransform.transform.rotation *= Quaternion.AngleAxis(_look.x * rotationPower, Vector3.up);
-        FollowTransform.transform.rotation *= Quaternion.AngleAxis(_look.y * rotationPower, -Vector3.right);
-
-        var angles = FollowTransform.transform.localEulerAngles;
-        angles.z = 0;
-
-        var angle = FollowTransform.transform.localEulerAngles.x;
-
-        //Clamp rotation
-        if (angle > 180 && angle < 340)
-            angles.x = 340;
-        else if(angle < 180 && angle > 40)
-            angles.x = 40;
-
-        FollowTransform.transform.localEulerAngles = angles;
     }
 }
